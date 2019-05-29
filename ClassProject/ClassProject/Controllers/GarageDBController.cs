79a31809a17aa446c1b0fed4570c1c645c1038c1
Parker Eastman <ParkerEastman@outlook.com>
2019-05-29 07:46:28 -0400

    restarted website, linked db, displayed expandable table, working on api w/ google maps

---
+++ ClassProject/ClassProject/Controllers/GarageDBController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ClassProject.Models;
+
+namespace ClassProject.Controllers
+{
+    public class GarageDBController : Controller
+    {
+        // GET: GarageDB
+        public ActionResult Index()
+        {
+            GarageDBEntities1 context = new GarageDBEntities1();
+            
+            return View(context.Lots);
+        }
+    }
+}
